91ecc800184c006bd011e99ec400ffeae2568a52
Jos77we <josemaina207@gmail.com>
2024-01-05 18:37:10 +0300

    major popup changes

--- src/Contents/NewTransaction.jsx
+++ src/Contents/NewTransaction.jsx
@@ -1,12 +1,131 @@
-import React from 'react'
+import React from "react";
+import "../Design/PopupWin.css";
+import ReceiverId from "../autoGenerate/ReceiverId";
+import GenTransaction from "../autoGenerate/GenTransaction";
 
 const NewTransaction = () => {
-    
   return (
-    <div className='form-input' style={{height:'80%'}}>
-        <p>NewTransaction</p>
+    <>
+      <div className="popup-input" style={{ height: "85%" }}>
+        <div style={{padding:'10px'}}>
+        <p style={{fontSize:'1.4rem', fontWeight:'620', marginLeft:'20px'}}>New Transaction</p>
         </div>
-  )
-}
+        <div
+          style={{
+            height: "100px",
+            display: "flex",
+            columnGap: "30%",
+          }}
+        >
+          <div>
+            <p>transactionId </p>
+            <div
+              style={{
+                height: "40px",
+                width: "160px",
+                marginLeft: "20px",
+                border: "none",
+                borderBottom: "2px solid white",
+                backgroundColor: "rgba(146, 146, 146, 0.30)",
+                outline: "none",
+              }}
+            >
+              <GenTransaction />
+            </div>
+          </div>
+          <div>
+            <p>recieverId,</p>
+            <div
+              style={{
+                height: "40px",
+                width: "160px",
+                marginLeft: "20px",
+                border: "none",
+                borderBottom: "2px solid white",
+                marginTop: "2px",
+                backgroundColor: "rgba(146, 146, 146, 0.30)",
+                outline: "none",
+              }}
+            >
+              <ReceiverId />
+            </div>
+          </div>
+        </div>
+        <div style={{height:'120px'}}>
+        <p>receiverAccountNo</p>
+        <input
+          style={{
+            height: "40px",
+            width: "80%",
+            marginLeft: "20px",
+            border: "none",
+            borderBottom: "2px solid white",
+            marginTop: "2px",
+            backgroundColor: "rgba(146, 146, 146, 0.30)",
+            outline: "none",
+          }}
+          // placeholder="Name"
+          type="text"
+        />
+        </div>
+       <div style={{height:'120px', columnGap:'30%', display:'flex'}}>
+        <div>
+        <p>amount,</p>
+        <input
+          style={{
+            height: "40px",
+            width: "150px",
+            marginLeft: "20px",
+            border: "none",
+            borderBottom: "2px solid white",
+            marginTop: "2px",
+            backgroundColor: "rgba(146, 146, 146, 0.30)",
+            outline: "none",
+          }}
+          // placeholder="Name"
+          type="text"
+        />
 
-export default NewTransaction
+        </div>
+        <div>
+        <p>typeOfTransaction,</p>
+        <input
+          style={{
+            height: "40px",
+            width: "70px",
+            marginLeft: "20px",
+            border: "none",
+            borderBottom: "2px solid white",
+            backgroundColor: "rgba(146, 146, 146, 0.30)",
+            outline: "none",
+          }}
+          // placeholder="Name"
+          type="text"
+        />
+        </div>
+       </div>
+        {/* <p>accountNo,</p>
+    <input
+          style={{
+            height: "40px",
+            width: "80%",
+            marginLeft: "20px",
+            border: "none",
+            borderBottom: "2px solid white",
+            marginTop: "2px",
+            backgroundColor: "rgba(146, 146, 146, 0.30)",
+            outline:'none'
+          }}
+          // placeholder="Name"
+          type="text"
+          
+        /> */}
+       
+        
+       
+      </div>
+    </>
+  );
+};
+
+export default NewTransaction;
